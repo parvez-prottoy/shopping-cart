9f643e2b78502abd834f50144823d38b9fe2078e
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2024-09-19 11:05:34 +0600

    adding class change to className

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,18 +1,18 @@
 const Navigation = () => {
   return (
-    <header class="fixed w-full z-10 text-gray-600 body-font border-b shadow-sm">
-      <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a class="flex font-monospace text-xl font-extrabold items-center text-gray-900 mb-4 md:mb-0">
+    <header className="fixed w-full z-10 text-gray-600 body-font border-b shadow-sm">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a className="flex font-monospace text-xl font-extrabold items-center text-gray-900 mb-4 md:mb-0">
           Shopping Cart
         </a>
-        <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-          <a class="mr-5 hover:text-gray-900">Home</a>
-          <a class="mr-5 hover:text-gray-900">Products</a>
-          <a class="mr-5 hover:text-gray-900">Blogs</a>
-          <a class="mr-5 hover:text-gray-900">Contact</a>
-          <a class="mr-5 hover:text-gray-900">About Us</a>
+        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+          <a className="mr-5 hover:text-gray-900">Home</a>
+          <a className="mr-5 hover:text-gray-900">Products</a>
+          <a className="mr-5 hover:text-gray-900">Blogs</a>
+          <a className="mr-5 hover:text-gray-900">Contact</a>
+          <a className="mr-5 hover:text-gray-900">About Us</a>
         </nav>
-        <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+        <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
           Cart(0) $0
         </button>
       </div>
--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -2,8 +2,8 @@ import Product from "./Product";
 
 const Products = () => {
   return (
-    <div class="container px-5 py-24 mx-auto">
-      <div class="flex flex-wrap -m-4">
+    <div className="container px-5 py-24 mx-auto">
+      <div className="flex flex-wrap -m-4">
         <Product />
         <Product />
         <Product />
